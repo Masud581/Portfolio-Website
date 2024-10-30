00c68ee53dbc26d76dbe7cfc3a5dbd781a0503a6
Ebne Masud <ebnemasud74@gmail.com>
2024-10-30 20:01:57 +0530

    Initial commit

---
+++ tailwind-project/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",     // Add all HTML files
+    "./style.css",      // Add your CSS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
